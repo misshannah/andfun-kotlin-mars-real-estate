4f3fbd071dba6b7de1ff77d112a0fa2fd3920158
Hannah Olukoye <hannah.olukoye@macro-eyes.com>
2020-09-02 18:24:05 +0300

    Feat: Making web service contact

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,8 +17,34 @@
 
 package com.example.android.marsrealestate.network
 
+import android.app.TaskStackBuilder.create
+import android.renderscript.RenderScript.create
+import retrofit2.Call
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
 
-// TODO (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
-// TODO (03) Implement the MarsApiService interface with @GET getProperties returning a String
-// TODO (04) Create the MarsApi object using Retrofit to implement the MarsApiService
+// DONE (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
+// DONE (03) Implement the MarsApiService interface with @GET getProperties returning a String
+// DONE (04) Create the MarsApi object using Retrofit to implement the MarsApiService
+
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            Call<String>
+}
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
+
